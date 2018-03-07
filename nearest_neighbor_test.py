be21d652630de5cd7dabdcd98cbd5e632e9286ac
Nathaniel Bennett <nathaniel.w.bennett@gmail.com>
2018-03-07 13:16:53 -0800

    Add getSmallest, remove city

--- nearest_neighbor_test.py
+++ nearest_neighbor_test.py
@@ -1,6 +1,7 @@
 import math
+import sys
 
-def getContent(fileName):
+def getContentAsList(fileName):
     allCities = []
     with open(fileName) as f:
         for line in f:
@@ -13,13 +14,81 @@ def getContent(fileName):
 
     return allCities
 
-def getDistance(city1, city2):
-    test = math.sqrt(city1[0] + city2[0])    
-    print(test)
+def getContent(fileName):
+    allCities = {}
+    with open(fileName) as f:
+        for line in f:
+            city = []
 
+            # Split each line into array
+            lineArr = line.split()
+            lineArr = list(map(int, lineArr))
+            
+            # Build x and y data
+            city.append(lineArr[1])
+            city.append(lineArr[2])
+            city = list(map(int, city))
+
+            allCities[lineArr[0]] = (city)
+
+    return allCities
+
+def getDistance(city1, city2):
+    x = (city1[0] - city2[0])**2
+    y = (city1[1] - city2[1])**2
+    distance = round(math.sqrt(x + y))
+    return distance
+
+# This is inefficient 
 def buildAdjacencyMatrix(data):
-    matrix = []
-    print (data)
+    myMatrix = []
+
+    for i in range(len(data)):
+        city = []
+        for y in range(len(data)):
+            city.append(getDistance(data[i], data[y]))
+        myMatrix.append(city)
+
+    print(myMatrix)
+    return myMatrix
+
+def getSmallest(cities, cityId):
+    smallestId = -1
+    smallest = sys.maxsize
+
+    for i in range(len(cities)):
+        distance = getDistance(cities[cityId], cities[i])
+        if distance < smallest and distance > 0:
+            print(distance)
+            smallest = distance
+            smallestId = i
+
+    path = [smallestId, smallest]
+    return path
+
+def removeCity(cities, index):
+    city = {index: cities[index]}
+    del cities[index]
+    return city
+
+def getPath(cities):
+    path = []
+    
+    path.append(removeCity(cities, 0))
+    print(path)
+
+    while cities:
+        smallest = -1
+        current = path[len(path) - 1]
+        for key in current:
+            current = key
+
+def deleteAll(cities):
+    i = 0
+    while cities:
+        removeCity(cities, i)
+        print(cities)
+        i += 1
 
 cities = getContent("./TSP_Files-1/tsp_example_1.txt")
-getDistance(cities[0], cities[1])
+getPath(cities)
