05a05714b6ed6b400f282ad542c48a908913a42c
Nathaniel Bennett <nathaniel.w.bennett@gmail.com>
2018-03-07 22:15:00 -0800

    Add last path distance

--- nearest_neighbor_test.py
+++ nearest_neighbor_test.py
@@ -1,20 +1,12 @@
+"""
+Nearest Neighbor implimentation for the Travelling Salesman Problem
+Project Group 31
+"""
 import math
 import sys
 
-def getContentAsList(fileName):
-    allCities = []
-    with open(fileName) as f:
-        for line in f:
-
-            # Split each line into array, convert to int
-            city = line.split()
-            city = list(map(int, city))
-            city.pop(0)
-            allCities.append(city)
-
-    return allCities
-
 def getContent(fileName):
+    """Get and store all city data from file."""
     allCities = {}
 
     with open(fileName) as f:
@@ -40,24 +32,10 @@ def getDistance(city1, city2):
     distance = round(math.sqrt(x + y))
     return distance
 
-# This is inefficient 
-def buildAdjacencyMatrix(data):
-    myMatrix = []
-
-    for i in range(len(data)):
-        city = []
-        for y in range(len(data)):
-            city.append(getDistance(data[i], data[y]))
-        myMatrix.append(city)
-
-    print(myMatrix)
-    return myMatrix
-
 def getSmallest(allCities, city):
     smallestId = -1
     smallest = sys.maxsize
     
-    # TODO: Iteration by index won't work.
     for i in allCities:
         distance = getDistance(city, allCities[i])
         if distance < smallest and distance > 0:
@@ -82,7 +60,7 @@ def getPath(cities, startingId):
     # Iterate through unvisited cities
     while cities:
         smallest = []
-        currentKey = -1
+        currentKey = None
         current = path[len(path) - 1]
         
         # Get id from current city
@@ -94,14 +72,12 @@ def getPath(cities, startingId):
         path.append(removeCity(cities, smallest[0]))
     
     # TODO: Add distance from last city to first city
-    return [distance, path]
+    # firstCity = path[0][startingId]
+    # lastCity = path[len(path) - 1]
 
-def deleteAll(cities):
-    i = 0
-    while cities:
-        removeCity(cities, i)
-        print(cities)
-        i += 1
+    # print(firstCity)
+    # print((lastCity.keys()))
+    return [distance, path]
 
 cities = getContent("./TSP_Files-1/tsp_example_2.txt")
 path = getPath(cities, 0)
